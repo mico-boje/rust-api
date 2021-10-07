0cdb4c4d88415164872dd7010f4c74dce9d70894
Mico Bøje <mico.boeje@gmail.com>
2021-10-07 22:01:22 +0100

    Updated bayes to use post and JSON body and changed multiple get requests to use parameters

--- src/bayes.rs
+++ src/bayes.rs
@@ -1,10 +1,32 @@
-use rocket::response::status;
+use rocket::serde::json::Json;
+//use rocket::form::Form;
+use serde::{Serialize, Deserialize};
 
-#[get("/bayes_theorem/<a>/<b>/<pba>")]
-pub fn bayes_theorem(a: f64, b: f64, pba: f64) -> status::Accepted<String> {
-    // Json({
-    //     "Probability": "(pba * a) / b"
-        
-    // })
-    status::Accepted(Some(format!("Probability: '{}'", (pba * a) / b)))
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BayesTheorem {
+    event_a: f64,
+    event_b: f64,
+    b_given_a: f64,
+    a_given_b: f64
 }
+
+
+// #[get("/bayes_theorem/<a>/<b>/<pba>")]
+// pub fn bayes_theorem(a: f64, b: f64, pba: f64) -> Json<BayesTheorem> {
+//     let bayes = BayesTheorem {event_a: a, event_b: b, b_given_a: pba, a_given_b: ((pba * a) / b), };
+//     Json(bayes)
+// }
+
+#[derive(Deserialize)]
+pub struct Input {
+    event_a: f64,
+    event_b: f64,
+    b_given_a: f64
+}
+
+#[post("/bayes_theorem", data = "<input>")]
+pub fn bayes_theorem(input: Json<Input>) -> Json<BayesTheorem> {
+    let bayes = BayesTheorem {event_a: input.event_a, event_b: input.event_b, b_given_a: input.b_given_a, a_given_b: ((input.b_given_a * input.event_a) / input.event_b), };
+    Json(bayes)
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+#![feature(decl_macro, proc_macro_hygiene)]
 #[macro_use] extern crate rocket;
-// #[macro_use] extern crate rocket_contrib;
-// #[macro_use] extern crate serde_derive;
+
+
 mod bayes;
 
 use rocket::tokio::time::{sleep, Duration};
 use rocket::Request;
+use rocket::get;
 use bayes::bayes_theorem;
 
+#[derive(serde::Serialize)]
+struct Response {
+    reply: String,
+}
 
-#[get("/delay/<seconds>")]
+#[get("/delay?<seconds>")]
 async fn delay(seconds: u64) -> String {
     sleep(Duration::from_secs(seconds)).await;
     format!("Waited for {} seconds", seconds)
@@ -19,7 +25,8 @@ fn world() -> &'static str {  // <- request handler
     "hello, world!"
 }
 
-#[get("/hello_person/<name>/<age>/<cool>")]
+
+#[get("/hello_person?<name>&<age>&<cool>")]
 fn hello(name: &str, age: u8, cool: bool) -> String {
     if cool {
         format!("You're a cool {} year old, {}!", age, name)
@@ -33,7 +40,7 @@ fn not_found(req: &Request) -> String {
     format!("Sorry, '{}' is not a valid path.", req.uri())
 }
 
-// #[launch] // No async variant
+// #[launch] // Non async variant
 // fn rocket() -> _ {
 //     rocket::build()
 //         .mount("/hello", routes![world])
